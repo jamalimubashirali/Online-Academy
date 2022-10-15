54f4b19b3512ceee8ec79287842d11a58e48cade
Mubashir <21sw071@students.muet.edu.pk>
2022-10-15 21:48:45 +0500

    commit

--- src/LogIn.java
+++ src/LogIn.java
@@ -3,20 +3,20 @@ public class LogIn {
    private String enteredName;
    private String enteredPassword;
    Register rg;
-    LogIn(){
+    LogIn() {
         boolean codition = true;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your Username or Email: ");
         enteredName = sc.next();
         System.out.println("Enter Your Password");
         enteredPassword = sc.next();
-        for (int i = 0;i< rg.userNames.size() ; i++){
-            if(enteredName.equals(rg.userNames.get(i))||enteredName.equals(rg.emails.get(i))){
+        for (int i = 0; i < rg.userNames.size(); i++) {
+            if (enteredName.equals(rg.userNames.get(i)) || enteredName.equals(rg.emails.get(i))) {
                 System.out.println("LogIn SuccessFull....");
-                codition=false;
+                codition = false;
                 break;
             }
-            if (codition==true) System.out.println(" Wrong Email or password ");
+            if (codition == true) System.out.println(" Wrong Email or password ");
         }
     }
 }
